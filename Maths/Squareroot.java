8b8bca04de32a2120ac844bf0401188e317cd117
Vaishnavidg <54136990+Vaishnavidg@users.noreply.github.com>
2023-01-06 19:30:50 +0530

    Add files via upload

---
+++ Maths/Squareroot.java
@@ -0,0 +1,38 @@
+package Maths;
+
+//Finding squareroot using binary search
+public class Squareroot {
+    public static void main(String[] args) {
+        int n=40;//number
+        int p = 3;//precision value
+        System.out.printf("%.3f",sqrt(n,p));
+
+    }
+    static double sqrt(int n ,int p){
+        int start =0;
+        int end = n;
+        double root =0.0;
+        while(start<=end){
+            int mid = start +(end - start)/2;
+            if(mid*mid ==n){
+                return mid;
+            }
+            if(mid*mid>n){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        double incr = 0.1;
+        for(int i=0;i<p;i++){
+            while(root*root<=n){
+                root +=incr;
+            }
+            root-=incr;
+            incr = incr/10;
+
+        }
+        return root;
+    }
+
+}
